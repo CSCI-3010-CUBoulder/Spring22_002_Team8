9a6ed3e60916100471582a91bb89ee5ed38a6054
davis-cohen-3 <davis.cohen@colorado.edu>
2022-02-08 12:43:41 -0700

    davis

--- functions_to_implement.cpp
+++ functions_to_implement.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <vector>
 /* String functions section */
 
 // Splits a single string on separator into a vector of strings
@@ -31,17 +33,21 @@ std::vector<bool> OddMask(std::vector<int>);
 
 // Sums all numbers in a vector and returns the resulting value
 int Sum(std::vector<int> nums){
-int sum = 0
-for(int i=0;i<nums.size();i++){
-	sum+=nums[i];}}
+	int sum = 0;
+	for(int i=0;i<nums.size();i++){
+		sum+=nums[i];
+	}
+}
 
 
 
 // Multiplies all numbers in a vector together and returns the resulting value
 int Product(std::vector<int> nums){
-int product = 1;
-for(int i=0; i<nums.size();i++){
-	product *= nums[i];}}
+	int product = 1;
+	for(int i=0; i<nums.size();i++){
+		product *= nums[i];
+	}
+}
 
 // Adds an integer n to each element of a given vector
 std::vector<int> VectorPlusN(std::vector<int> v, int n);
