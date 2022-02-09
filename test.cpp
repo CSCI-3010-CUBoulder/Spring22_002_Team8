939f6703bdee7f3b757178e050a9e7b3ff905342
danaalasfour <daal5645@colorado.edu>
2022-02-09 00:55:17 +0000

    pushed changes

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include <vector>
+#include "catch.hpp"
+#include "functions_to_implement.cpp"
+#define CATCH_CONFIG_MAIN
+
+
+unsigned int Factorial( unsigned int number ) {
+  return number > 1 ? Factorial(number-1)*number : 1;
+}
+
+TEST_CASE( "Factorials are computed", "[factorial]" ) {
+    REQUIRE( Factorial(1) == 1 );
+    REQUIRE( Factorial(2) == 2 );
+    REQUIRE( Factorial(3) == 6 );
+    REQUIRE( Factorial(10) == 3628800 );
+}
+
+
